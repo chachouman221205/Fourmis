d8fa573839802d8c9a2518568d6bbafd4882a980
chachouman <boudekhani.nassim@gmail.com>
2024-12-30 14:39:56 +0100

    ajout de search_object

--- objects.c
+++ objects.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <string.h>
 #include "simulation.h"
 
 
@@ -27,6 +29,15 @@ Object* init_object(Simulation_data* simulation_data, char* name_ID, int size, b
     }
 
     return new_obj;
+}
+
+Object* search_object(Room* room, char* NameID) {
+    for (int i = 0; i < room->Obj_count; i++) {
+        if (strcmp(room->Obj_list[i]->Name_ID, NameID) == 0) {
+            return room->Obj_list[i];
+        }
+    }
+    return NULL;
 }
 
 void free_object(Simulation_data* simulation_data, Object* object){
