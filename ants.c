c08cd90f7ce7bba19e69784c85fac86f26987049
chachouman <boudekhani.nassim@gmail.com>
2025-01-01 10:54:12 +0100

    init_new_ant copie les propriétés de larve

--- ants.c
+++ ants.c
@@ -261,9 +261,9 @@ Ant* init_new_ant(Simulation_data* simulation_data, Larve* larve) {
 
     simulation_data->ant_NB++;
 
-    new_ant->PV = larve->Nest->PV[larve->Ant_type];
-    new_ant->DMG = larve->Nest->DMG[larve->Ant_type];
-    new_ant->Hunger = larve->Nest->Hunger - (larve->Hunger / 2);
+    new_ant->PV = larve->PV;
+    new_ant->DMG = larve->DMG;
+    new_ant->Hunger = larve->Hunger;
     new_ant->Life = larve->Nest->Life_min + rand() % (larve->Nest->Life_max - larve->Nest->Life_min + 1); // Life entre Life_min et Life_max
     new_ant->Position = larve->Position;     // Position NULL au départ, assignation plus tard
     new_ant->Held_object = NULL;  // Pas d'objet au départ
